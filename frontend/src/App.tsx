ff056016cbb189f1cdf6b089befb5c5633df2263
Sillas Cavalcante <sillas.ap16@gmail.com>
2021-05-09 01:59:58 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import BarChart from 'components/bar-chart';
 import DataTable from 'components/data-table';
+import DonutChart from 'components/donut-chart';
 import Footer from 'components/footer';
 import Navbar from 'components/navbar';
 
@@ -7,8 +9,23 @@ function App() {
     <>
       <Navbar />
       <div className="container">
-        <h1 className="text-primary">Olá mundo</h1>
+        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
 
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
+            <BarChart />
+            <DonutChart />
+          </div>
+        </div>
+
+        <div className="py-3">
+          <h2 className="text-primary">Todas as vendas</h2>
+        </div>
         <DataTable />
       </div>
       <Footer />
