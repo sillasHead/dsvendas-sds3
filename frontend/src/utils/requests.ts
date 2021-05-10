59424f388f87672bba0e413f7cbfa10de220a92b
Sillas Cavalcante <sillas.ap16@gmail.com>
2021-05-10 04:49:24 -0300

    Env var

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,1 +1,3 @@
-export const BASE_URL = 'http://localhost:8080';
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+//§ se a url da variavel de ambiente no netlify (BACKEND_URL), for undefined, utilizar localhost:8080
+//obs: REACT_APP eh padrao do netlify
