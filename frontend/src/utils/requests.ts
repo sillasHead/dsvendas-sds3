227be23886e41a2f11fb03d7d47a559813e8dbd0
Sillas Cavalcante <sillas.ap16@gmail.com>
2021-05-10 05:47:29 -0300

    bug fixes

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,3 +1,4 @@
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+export const BASE_URL = 'https://sillashead-dsvendas.herokuapp.com';
+//process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
 //§ se a url da variavel de ambiente no netlify (BACKEND_URL), for undefined, utilizar localhost:8080
 //obs: REACT_APP eh padrao do netlify
