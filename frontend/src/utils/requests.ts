40130bf6d305e94b06c0da0bb56f68ad02ebe14c
Sillas Cavalcante <sillas.ap16@gmail.com>
2021-05-10 06:20:23 -0300

    Environment variable test

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,4 +1,2 @@
-export const BASE_URL = 'https://sillashead-dsvendas.herokuapp.com';
-//process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
-//§ se a url da variavel de ambiente no netlify (BACKEND_URL), for undefined, utilizar localhost:8080
-//obs: REACT_APP eh padrao do netlify
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+//§ se a url da variavel de ambiente no netlify (REACT_APP_BACKEND_URL), for undefined, utilizar localhost:8080
